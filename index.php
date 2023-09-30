b76382535c0ff353d62cd79f666f7d13f8609f3d
krishnadev7 <krishnadevv12@gmail.com>
2023-09-30 12:32:15 +0530

    user singup page added

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <title>Login -Online voting web app</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
-<!--Coded with love by Mutiullah Samim-->
 
 <body>
     <div class="container h-100">
@@ -17,34 +17,81 @@
                         <img src="assets/images/vote-logo.jpg" class="brand_logo" alt="Logo">
                     </div>
                 </div>
-                <div class="d-flex justify-content-center form_container">
-                    <form>
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" name="" class="form-control input_user" value="" placeholder="username">
-                        </div>
-                        <div class="input-group mb-2">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                            </div>
-                            <input type="password" name="" class="form-control input_pass" value="" placeholder="password">
-                        </div>
-                        <div class="d-flex justify-content-center mt-3 login_container">
-                            <button type="button" name="button" class="btn login_btn">Login</button>
-                        </div>
-                    </form>
-                </div>
 
-                <div class="mt-4">
-                    <div class="d-flex justify-content-center links">
-                        Don't have an account? <a href="#" class="ml-2">Sign Up</a>
+                <?php
+                if (isset($_GET['sign-up'])) {
+                ?>
+                    <div class="d-flex justify-content-center form_container">
+                        <form method="POST">
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="email" name="su_emailId" class="form-control input_user" value="" placeholder="e-mail Id">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="number" name="su_phoneNo" class="form-control input_user" value="" inputmode="numeric" placeholder="phone No.">
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input type="password" name="su_pass" class="form-control input_pass" value="" placeholder="password">
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input type="password" name="su_cPass" class="form-control input_pass" value="" placeholder="confirm password">
+                            </div>
+                            <div class="d-flex justify-content-center mt-3 login_container">
+                                <button type="button" name="signUp-btn" class="btn login_btn">Submit</button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="d-flex justify-content-center links">
-                        <a href="#">Forgot your password?</a>
+
+                    <div class="mt-4">
+                        <div class="d-flex justify-content-center links">
+                            Already have an account? <a href="index.php" class="ml-2">Sign In</a>
+                        </div>
                     </div>
-                </div>
+                <?php
+                } else {
+                ?>
+                    <div class="d-flex justify-content-center form_container">
+                        <form>
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="" class="form-control input_user" value="" placeholder="username">
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input type="password" name="" class="form-control input_pass" value="" placeholder="password">
+                            </div>
+                            <div class="d-flex justify-content-center mt-3 login_container">
+                                <button type="button" name="button" class="btn login_btn">Login</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="mt-4">
+                        <div class="d-flex justify-content-center links">
+                            Don't have an account? <a href="?sign-up=1" class="ml-2">Sign Up</a>
+                        </div>
+                        <div class="d-flex justify-content-center links">
+                            <a href="#">Forgot your password?</a>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
@@ -54,3 +101,35 @@
 </body>
 
 </html>
+
+<?php
+require_once('admin/includes/config.php');
+
+// if (isset($_POST['signUp-btn'])) {
+
+//     $su_emailId = mysqli_escape_string($db, $_POST['su_emailId']);
+//     $su_phoneNo = mysqli_escape_string($db, $_POST['su_phoneNo']);
+//     $su_pass = mysqli_escape_string($db, $_POST['su_pass']);
+//     $su_cPass = mysqli_escape_string($db, $_POST['su_cPass']);
+//     $su_user_role = 'voter';
+
+//     if ($su_pass == $su_cPass) {
+//         // insert Query
+//         mysqli_query($db, "INSERT INTO users(username,phone_no,password,user_role) VALUES('" . $su_emailId . "','" . $su_phoneNo . "','" . $su_pass . "','" . $user_role . "')")
+//             or die(mysqli_error($db));
+
+// ?>
+//         <script>
+//             location.assign('index.php?sign-up=1&registered=1');
+//         </script>
+//     <?
+
+//     } else {
+//     ?>
+//         <script>
+//             location.assign('index.php?sign-up=1&invalid=1');
+//         </script>
+// <?
+//     }
+// }
+?>
